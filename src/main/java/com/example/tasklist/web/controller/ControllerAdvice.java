af65b8cc8736a1dfc803b8a9e968859030006ea1
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-07-30 00:43:03 +0300

    Uncommented annotation

--- src/main/java/com/example/tasklist/web/controller/ControllerAdvice.java
+++ src/main/java/com/example/tasklist/web/controller/ControllerAdvice.java
@@ -12,11 +12,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-//@RestControllerAdvice
+@RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(ResourceNotFoundException.class)
